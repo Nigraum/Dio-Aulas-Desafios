05f48804e300c04a2d5ddd616b1af4f64d4006da
Nigraum <yan.corintima@hotmail.com>
2022-03-30 17:41:22 -0300

    Arquivo do exercício sobre Debbugin/Error adicionado

---
+++ JavaScript/validaArrays.js
@@ -0,0 +1,30 @@
+function validaArray(arr, num){
+    try{
+        if (!arr && !num) throw new ReferenceError("Envie os parâmetros");
+
+    if (typeof arr !== 'object') 
+     throw new TypeError("O array precisa ser do tipo Object");
+
+    if (typeof num !== 'number') 
+     throw new TypeError("O array precisa ser do tipo number");
+
+    if (arr.length !== num) throw new RangeError("Tamanha incorreto");
+
+    return arr;
+    }
+    catch(e){
+        if (e instanceof ReferenceError){
+            console.log("É um ReferenceError")
+            console.log(e.message)
+        } else if (e instanceof TypeError){
+            console.log("É um TypeError")
+            console.log(e.message)
+        } else if(e instanceof RangerError){
+            console.log("É um RangeError!")
+        } else {
+            console.log("Um erro não esperado" + e);
+        }
+    }
+
+}
+console.log(validaArray())
