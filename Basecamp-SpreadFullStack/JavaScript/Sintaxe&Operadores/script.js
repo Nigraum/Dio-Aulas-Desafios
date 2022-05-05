6bca59a816af7c479243759b758770dac5464d69
Nigraum <yan.corintima@hotmail.com>
2022-05-05 05:02:41 -0300

    Corrigindo exercicio de Sintaxe e Operadores

--- Basecamp-SpreadFullStack/JavaScript/Sintaxe&Operadores/script.js
+++ Basecamp-SpreadFullStack/JavaScript/Sintaxe&Operadores/script.js
@@ -1,35 +1,35 @@
-function comparadorNumeros (num1, num2) {
-    const primeiraFrase = criaPrimeiraFrase(num1, num2);
-    const segundaFrase = criaSegundaFrase(num1, num2);
+function comparaNumeros(num1, num2) {
+	const primeiraFrase = criaPrimeiraFrase(num1, num2);
+	const segundaFrase = criaSegundaFrase(num1, num2);
 
-    return '${primeiraFrase} ${segundaFrase}'
+	return `${primeiraFrase} ${segundaFrase}`;
 }
 
 function criaPrimeiraFrase(num1, num2) {
-    let saoIguais = '';
+	let primeiraFrase = `Os números ${num1} e ${num2}`;
+	let simNao = 'não';
 
-    if (num1 != num2){
-        saoIguais = 'São';
-    }
+	if (num1 === num2) {
+		simNao = '';
+	}
 
-    return 'Os números ${num1} e ${num2} $ {saoIguais} são iguais.'
+	return `${primeiraFrase} ${simNao} são iguais.`;
 }
 
-function criaSegundaFrase (num1, num2) {
-    const soma = num1 + num2;
+function criaSegundaFrase(num1, num2) {
+	const soma = num1 + num2;
+	let comparaDez = 'menor';
+	let comparaVinte = 'menor';
 
-    let resultado10 = 'menor';
-    let resultado20 = 'menor';
-    const compara10 = soma > 10;
-    const compara20 = soma > 20;
+	if (soma > 10) {
+		comparaDez = 'maior';
+	}
 
-    if (compara10) {
-        resultado10 = 'maior'
-    }
+	if (soma > 20) {
+		comparaVinte = 'maior';
+	}
 
-    if (compara20) {
-        resultado20 = 'maior';
-    }
-
-    return 'Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.'
+	return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`;
 }
+
+console.log(comparaNumeros(9, 5));
