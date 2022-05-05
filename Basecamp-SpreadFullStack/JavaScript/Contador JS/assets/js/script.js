c6e13b515c9bdf2f9d19f746226b7c19d49774b6
Nigraum <yan.corintima@hotmail.com>
2022-05-05 04:57:24 -0300

    Pasta com exercicio de Sintaxe e operadores

--- Basecamp-SpreadFullStack/JavaScript/Contador JS/assets/js/script.js
+++ Basecamp-SpreadFullStack/JavaScript/Contador JS/assets/js/script.js
@@ -4,9 +4,9 @@ var currentNumber = 0;
 function increment () {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
-}
+};
 
 function decrement () {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
-}
+};
