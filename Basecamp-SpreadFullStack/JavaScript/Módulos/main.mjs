9d5685966f87d88c0eb7127fab31686bce731a4f
Nigraum <yan.corintima@hotmail.com>
2022-05-08 06:07:40 -0300

    Adicionando exercicio de modulos

---
+++ Basecamp-SpreadFullStack/JavaScript/Módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./functions.mjs";
+
+console.log(mostraIdade("Yan", 25));
+console.log(mostraCidade("Yan", "São Paulo"));
+console.log(mostraHobby("Yan", "Tennis"));
