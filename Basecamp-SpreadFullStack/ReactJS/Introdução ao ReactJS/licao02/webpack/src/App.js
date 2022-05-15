e9883f7bb48d1b60eba816a887ac825f6e32646e
Yan Corintima <nigraum@pop-os.localdomain>
2022-05-15 16:08:13 -0300

    Adicionando exercicio de Webpack

---
+++ Basecamp-SpreadFullStack/ReactJS/Introdução ao ReactJS/licao02/webpack/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+const App = () => {
+  return (
+    <div>
+      <p>Yan Corintima</p>
+      <p> Bem vindo a minha aula sobre WEBPACK</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ Basecamp-SpreadFullStack/ReactJS/Introdução ao ReactJS/licao02/webpack/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebPackPLugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundler.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPLugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
