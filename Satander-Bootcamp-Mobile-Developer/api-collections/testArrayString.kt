c18f7809dd0bc9390d4d80532f55eda26aecbd0a
Nigraum <yan.corintima@hotmail.com>
2022-06-09 18:29:41 -0300

    Forms to do a string array in kotlin

--- Satander-Bootcamp-Mobile-Developer/api-collections/testArrayString.kt
+++ Satander-Bootcamp-Mobile-Developer/api-collections/testArrayString.kt
@@ -7,4 +7,14 @@ fun main() {
     for (nome in nomes) {
         println(nome)
     }
+
+    println("-------------------")
+    nomes.sort()
+    nomes.forEach {println(it)}
+
+    val nomes2 = arrayOf("Jorge", "Douglas", "Fabin")
+
+    println("-------------------")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
 }
