145b713a350baa4f739452b74003dc46c8421c51
Nigraum <yan.corintima@hotmail.com>
2022-06-09 19:18:46 -0300

    Examples on DoubleArray

--- Satander-Bootcamp-Mobile-Developer/api-collections/testDoubleArray.kt
+++ Satander-Bootcamp-Mobile-Developer/api-collections/testDoubleArray.kt
@@ -12,4 +12,10 @@ fun main() {
     salarios[index] = salario * 1.1
     }
     salarios.forEach {println(it)}
+
+    println("----------------------")
+
+    val salarios2 = doubleArrayOf(1500.0, 1250.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach {println(it)}
 }
