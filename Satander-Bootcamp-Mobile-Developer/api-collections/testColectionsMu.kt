edebfac66ecb1a1c3439585ce8962a9d5624de97
Nigraum <yan.corintima@hotmail.com>
2022-06-10 17:54:52 -0300

    Remove and add features learn

--- Satander-Bootcamp-Mobile-Developer/api-collections/testColectionsMu.kt
+++ Satander-Bootcamp-Mobile-Developer/api-collections/testColectionsMu.kt
@@ -3,7 +3,15 @@ fun main() {
     val pedro = Funcionario ( "Pedro", 1500.0 )
     val maria = Funcionario ("Maria", 4000.0)
 
-    val funcionarios = mutableListOf(joao, maria, pedro)
+    val funcionarios = mutableListOf(joao, maria)
+    funcionarios.forEach {println(it)}
+
+    println("------------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach {println(it)}
+
+    println("------------------")
+    funcionarios.remove(joao)
     funcionarios.forEach {println(it)}
 }
 
